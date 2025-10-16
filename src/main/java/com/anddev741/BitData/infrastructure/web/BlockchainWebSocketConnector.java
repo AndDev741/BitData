ea948980d5a439ec6343c47f789ee1e04e28730e
AndDev741 <anddesenvolved@gmail.com>
2025-10-16 08:21:10 +0100

    feat/test: Adding retry in the websocket and a test to test... the retry

--- src/main/java/com/anddev741/BitData/infrastructure/web/BlockchainWebSocketConnector.java
+++ src/main/java/com/anddev741/BitData/infrastructure/web/BlockchainWebSocketConnector.java
@@ -1,12 +1,14 @@
 package com.anddev741.BitData.infrastructure.web;
 
 import java.net.URI;
+import java.time.Duration;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 
 import com.anddev741.BitData.domain.port.in.ReceiveTransactionsPort;
+import com.anddev741.BitData.infrastructure.config.WebSocketMetrics;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,9 +22,25 @@ public class BlockchainWebSocketConnector implements ReceiveTransactionsPort {
 
     private final ReactorNettyWebSocketClient client; 
     private final String BLOCKCHAIN_URI = "wss://ws.blockchain.info/inv";
+    private final WebSocketMetrics metrics;
 
     @Override
     public Flux<String> receiveUnconfirmedTransactions() {
+        return Flux.defer(this::connectToBlockchain)
+            .onErrorResume(error -> {
+                log.warn("[WEB] Error detected, retrying in 5 seconds...", error);
+
+                metrics.incrementWebSocketRetries();
+
+                return Mono.delay(Duration.ofSeconds(5))
+                    .thenMany(receiveUnconfirmedTransactions());
+            })
+            .doOnComplete(() -> {
+                log.info("[WEB] Connection completed, restarting in 5 seconds...");
+            });
+    }
+
+    private Flux<String> connectToBlockchain() {
         return Flux.create(sink -> {
             log.info("[WEB] Connecting to Blockchain WebSocket...");
 
@@ -35,7 +53,10 @@ public class BlockchainWebSocketConnector implements ReceiveTransactionsPort {
                         .map(WebSocketMessage::getPayloadAsText)
                         .doOnNext(sink::next)
                         .doOnError(sink::error)
-                        .doOnComplete(sink::complete)   
+                        .doOnComplete(() -> {
+                            log.info("[WEB] WebSocket connection closed");
+                            sink.complete();
+                        })
                 )
                 .then();
             })
--- src/test/java/com/anddev741/BitData/unit/BlockchainWebSocketConnectorTest.java
+++ src/test/java/com/anddev741/BitData/unit/BlockchainWebSocketConnectorTest.java
@@ -1,30 +1,40 @@
 package com.anddev741.BitData.unit;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.net.URI;
+import java.time.Duration;
 
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 
+import com.anddev741.BitData.infrastructure.config.WebSocketMetrics;
 import com.anddev741.BitData.infrastructure.web.BlockchainWebSocketConnector;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 public class BlockchainWebSocketConnectorTest {
     
     @Test
     public void shouldReceiveFluxMessages(){
+        Hooks.onErrorDropped(_ -> {});
         //Arrange
         ReactorNettyWebSocketClient mockClient = mock(ReactorNettyWebSocketClient.class);
         WebSocketSession mockSession = mock(WebSocketSession.class);
+        WebSocketMetrics mockMetrics = mock(WebSocketMetrics.class);
 
         WebSocketMessage msg1 = mock(WebSocketMessage.class);
         WebSocketMessage msg2 = mock(WebSocketMessage.class);
@@ -39,7 +49,7 @@ public class BlockchainWebSocketConnectorTest {
             return handler.handle(mockSession);
         });
 
-        BlockchainWebSocketConnector connector = new BlockchainWebSocketConnector(mockClient);
+        BlockchainWebSocketConnector connector = new BlockchainWebSocketConnector(mockClient, mockMetrics);
 
         //Act
 
@@ -48,6 +58,56 @@ public class BlockchainWebSocketConnectorTest {
         //Assert
         StepVerifier.create(flux.take(2))
             .expectNext("tx1", "tx2")
-            .verifyComplete();
+            .expectComplete();
+        
+    }
+
+    @Test
+    public void shouldRetryOnError(){
+        Hooks.onErrorDropped(_ -> {});
+        //Arrange
+        VirtualTimeScheduler.getOrSet();
+
+        ReactorNettyWebSocketClient mockClient = mock(ReactorNettyWebSocketClient.class);
+        WebSocketSession mockSession = mock(WebSocketSession.class);
+        WebSocketMetrics mockMetrics = mock(WebSocketMetrics.class);
+        WebSocketMessage msg = mock(WebSocketMessage.class);
+        when(msg.getPayloadAsText()).thenReturn("tx_after_retry");
+
+        when(mockSession.send(any())).thenReturn(Mono.empty());
+        when(mockSession.receive()).thenReturn(Flux.just(msg));
+
+        //first try -> error
+        //second try -> success
+        when(mockClient.execute(any(URI.class), any())).thenAnswer(new Answer<Mono<Void>>() {
+            private boolean first = true;
+            @Override
+            public Mono<Void> answer(InvocationOnMock invocation){
+                if(first) {
+                    first = false;
+                    return Mono.error(new RuntimeException("Simulated connection error"));
+                } else {
+                    WebSocketHandler handler = invocation.getArgument(1);
+                    return handler.handle(mockSession);
+                }
+            }
+        });
+
+        BlockchainWebSocketConnector connector = new BlockchainWebSocketConnector(mockClient, mockMetrics);
+
+        //Act
+
+        Flux<String> flux = connector.receiveUnconfirmedTransactions();
+
+        //Assert
+
+        StepVerifier.withVirtualTime(() -> flux)
+            .thenAwait(Duration.ofSeconds(5)) //retry delay
+            .expectNext("tx_after_retry")
+            .thenCancel()
+            .verify();
+
+        verify(mockMetrics, atLeastOnce()).incrementWebSocketRetries();
+     
     }
 }
