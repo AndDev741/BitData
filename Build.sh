9b5a266be079fcef1cb89b91571aa49c7e0cddca
AndDev741 <anddesenvolved@gmail.com>
2025-11-14 07:51:54 +0000

    feat: Adding commentary to facilitate build tests

--- Build.sh
+++ Build.sh
@@ -4,3 +4,5 @@ echo "Cleaning and building"
 echo "Installing project"
 echo "Building docker image"
 mvn clean package && docker build -t bitdata_app -f Dockerfile . && docker compose down --remove-orphans &&  docker compose up
+
+## -Dmaven.test.skip=true -DskipTests
